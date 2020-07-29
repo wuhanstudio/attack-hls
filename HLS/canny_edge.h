a7ccfc846ba13f6c63027f019f398983e333a183
Wu Han <wuhanstudio@qq.com>
2020-07-29 21:39:53 +0800

    Add Lenet

---
+++ HLS/canny_edge.h
@@ -0,0 +1,19 @@
+/*
+The canny_edge IP is based on the work of Aadeetya Shreedhar and Alexander Wang
+https://github.com/ka367/Lane-Detecting-Using-Hough-Transform/blob/master/test.cpp
+*/
+#include "hls_video.h"
+#include <ap_fixed.h>
+#include "iostream"
+#include "ap_int.h"
+#include <string.h>
+
+
+struct wide_stream {
+	float data;
+	ap_uint<1> user;
+	ap_uint<1> last;
+};
+
+
+void canny_edge(wide_stream* in_stream, wide_stream* out_stream);
